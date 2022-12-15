8033db93d41e7c2c64db2c90638469a0b789bc37
kkchart9 <koudai200109@gmail.com>
2022-12-15 17:39:50 +0900

    Modify carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,29 +3,29 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 
-CarrierWave.configure do |config|
-  config.storage :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_directory  = 'studymanagement'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: "AKIAZ67VTXJXPKB5Q7MO",
-    aws_secret_access_key: "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",
-    region: "ap-northeast-1",
-    path_style: true
-  }
-
-end
-
-# if Rails.env.production?
-#   CarrierWave.configure do |config|
-#     config.fog_credentials = {
-#       # Amazon S3用の設定
-#       :provider              => "AWS",
-#       :region                => "ap-northeast-1",
-#       :aws_access_key_id     => "AKIAZ67VTXJXPKB5Q7MO",
-#       :aws_secret_access_key => "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"
-#     }
-#     config.fog_directory     =  'studymanagementapp'
-#   end
+# CarrierWave.configure do |config|
+#   config.storage :fog
+#   config.fog_provider = 'fog/aws'
+#   config.fog_directory  = 'studymanagement'
+#   config.fog_credentials = {
+#     provider: 'AWS',
+#     aws_access_key_id: "AKIAZ67VTXJXPKB5Q7MO",
+#     aws_secret_access_key: "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",
+#     region: "ap-northeast-1",
+#     path_style: true
+#   }
+#
 # end
+
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      # Amazon S3用の設定
+      :provider              => "AWS",
+      :region                => "ap-northeast-1",
+      :aws_access_key_id     => "AKIAZ67VTXJXPKB5Q7MO",
+      :aws_secret_access_key => "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"
+    }
+    config.fog_directory     =  'studymanagement'
+  end
+end
