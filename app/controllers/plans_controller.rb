e8dc3c02e0001a39185754b6b7b26191a926a718
kkchart9 <koudai200109@gmail.com>
2023-02-13 22:48:39 +0900

    プランを登録する際に、投稿するように変更

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -9,20 +9,22 @@ class PlansController < ApplicationController
 
   def create
     day_of_week = plan_params["plan_day_of_week"]
-    pre_plan_data = current_user.plans.where(plan_day_of_week: day_of_week)
+    pre_plan_data = current_user.plans.find_by(plan_day_of_week: day_of_week)
+    content_plan = get_content_plan_text(pre_plan_data, plan_params)
+    @microposts = current_user.microposts.build(content: content_plan)
 
-    if pre_plan_data.empty?
+    if pre_plan_data.nil?
       @plan = current_user.plans.build(plan_params)
-      if @plan.save
-        flash[:info] = "登録しました。"
+      if @plan.save and @microposts.save
+        flash[:success] = "登録しました。"
         redirect_to plans_path
       else
         flash[:danger] = "登録に失敗しました。"
         redirect_to plans_path
       end
     else
-      if pre_plan_data.update(plan_params)
-        flash[:info] = "データを更新しました。"
+      if pre_plan_data.update(plan_params) and @microposts.save
+        flash[:success] = "データを更新しました。"
         redirect_to plans_path
       else
         flash[:danger] = "データの更新に失敗しました。"
@@ -41,6 +43,17 @@ class PlansController < ApplicationController
     params.require(:plan).permit(:plan_time_hour, :plan_time_minute, :plan_day_of_week)
   end
 
+  def get_time_text(hour, minute)
+    hour = hour.to_s
+    minute = minute.to_s
+
+    if minute.length == 1
+      minute_tx = "0" + minute
+    end
+    time_tx = hour.to_s + '時間' + minute_tx.to_s + '分'
+    return  time_tx
+  end
+
 
   def get_plan_week(plans)
     plan_week = {}
@@ -51,5 +64,26 @@ class PlansController < ApplicationController
     end
 
     return plan_week
+  end
+
+
+  def get_content_plan_text(pre_plan_data, plan_data)
+    youbi = ['日', '月', '火', '水', '木', '金', '土']
+
+    if pre_plan_data.nil?
+      time = get_time_text(plan_data[:plan_time_hour], plan_data[:plan_time_minute])
+      text = youbi[plan_data[:plan_day_of_week].to_i] + "を" + time+  "に新規登録しました。"
+    else
+      pre_time = get_time_text(pre_plan_data[:plan_time_hour], pre_plan_data[:plan_time_minute])
+      time = get_time_text(plan_data[:plan_time_hour], plan_data[:plan_time_minute])
+
+      if pre_plan_data[:plan_time_hour] == 0 and pre_plan_data[:plan_time_minute] == 0
+        text = youbi[plan_data[:plan_day_of_week].to_i] + "を" + time+  "に新規登録しました。"
+      else
+        text = youbi[plan_data[:plan_day_of_week].to_i] + "曜日を" + pre_time + "から"+ time + "に変更しました。"
+      end
+    end
+
+    return text
   end
 end
